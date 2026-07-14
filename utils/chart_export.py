fa442e6a913174cb19800d395480b0495b052e1b
natsuko-teramoto <saiyo@appdate-hd.co.jp>
2026-07-14 15:01:03 +0900

    Fix Japanese fonts in PowerPoint charts

--- utils/chart_export.py
+++ utils/chart_export.py
@@ -1,8 +1,14 @@
 from pathlib import Path
 
+PLOTLY_FONT_FAMILY = (
+    "Noto Sana CJK JP,"
+    "Noto Sana JP,"
+    "Yu Gothic,"
+    "Meiryo,"
+    "sans-serif"
+)
 
 IMAGE_DIR = Path("output/images")
-
 
 def get_chart_file_name(name):
     return (
@@ -58,6 +64,38 @@ def generate_ppt_images(chart_figures):
 
         if fig is None:
             continue
+        fig.update_layout(
+            font=dict(
+                family=PLOTLY_FONT_FAMILY,
+            ),
+            legend=dict(
+                font=dict(
+                    family=PLOTLY_FONT_FAMILY,
+                ),
+                title_font=dict(
+                    family=PLOTLY_FONT_FAMILY,
+                ),
+            ),
+        )
+
+        fig.update_xaxes(
+            tickfont=dict(
+                family=PLOTLY_FONT_FAMILY,
+            ),
+            title_font=dict(
+                family=PLOTLY_FONT_FAMILY,
+            ),
+        )
+
+        fig.update_yaxes(
+            tickfont=dict(
+                family=PLOTLY_FONT_FAMILY,
+            ),
+            title_font=dict(
+                family=PLOTLY_FONT_FAMILY,
+            ),
+        )
+
 
         saved_files.append(
             save_chart(
